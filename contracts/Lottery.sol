8023b42f948b5a993c8d69ce32b88bf5908dfc19
Jonathan Stahl <jonathan.stahl@uzh.ch>
2019-05-13 10:40:47 +0200

    add lottery states

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,21 +1,33 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 contract Lottery {
-  // Map holding tickets and buyer addresses
-  mapping(address => uint8[]) soldTickets;
+  enum State { Closed, Open } // Define states as enum
+
+  State lotteryState;
+  mapping(address => uint8[]) soldTickets; // Map holding tickets and buyer addresses
+
+  constructor() public {
+    lotteryState = State.Closed;
+  }
 
   function buyTicket(uint8 _ticketNr) public payable {
-    require(msg.value >= 1);
+    require(_ticketNr < 251 && _ticketNr > 0, "Ticket number must be in range [0, 250]"); // Prevent overflow of uint8
+    require(msg.value >= 1, "A ticket costs 1 Ether"); // Checks if enough ether is sent to buy a lottery ticket
     soldTickets[msg.sender].push(_ticketNr);
   }
 
-  function getTickets() public view returns (uint8[] memory) {
+  function getOwnTickets() public view returns (uint8[] memory) {
     return soldTickets[msg.sender];
   }
 
-  /* function evaluteWinner() public view {
+  function getLotteryState() public view returns (State) {
+    return lotteryState;
+  }
 
-  } */
+  function setLotteryState(State _lotteryState) public {
+    lotteryState = _lotteryState;
+  }
+
 }
 
 contract RandomNumberGenerator {
