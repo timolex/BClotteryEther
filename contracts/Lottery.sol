f82a33664ca636ac2c019cd260c75dae05023e1f
Fabio Maddaloni <fabio.maddaloni@hispeed.ch>
2019-05-13 14:57:02 +0200

    Winner is now determined and the funds are separated accordingly.

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -5,6 +5,9 @@ contract Lottery {
 
   State lotteryState;
   mapping(address => uint8[]) soldTickets; // Map holding tickets and buyer addresses
+  mapping(uint8 => address payable[]) playersByTicket; // Map holding tickets and buyer addresses
+  uint256 public amountPerAddress;
+  uint8 public winningNr;
 
   constructor() public {
     lotteryState = State.Closed;
@@ -14,6 +17,7 @@ contract Lottery {
     require(_ticketNr < 251 && _ticketNr > 0, "Ticket number must be in range [0, 250]"); // Prevent overflow of uint8
     require(msg.value >= 1, "A ticket costs 1 Ether"); // Checks if enough ether is sent to buy a lottery ticket
     soldTickets[msg.sender].push(_ticketNr);
+    playersByTicket[_ticketNr].push(msg.sender);
   }
 
   function getOwnTickets() public view returns (uint8[] memory) {
@@ -28,6 +32,29 @@ contract Lottery {
     lotteryState = _lotteryState;
   }
 
+  function getPotOfLotteryRound() public view returns (uint256) {
+    return (address(this).balance/(1 ether));
+  }
+
+  function getWinners() public payable {
+    //uint8 winningNr = uint8(uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty)))%251) + 1;
+    winningNr = 5;
+
+    if (playersByTicket[winningNr].length > 0) {
+      amountPerAddress = address(this).balance / playersByTicket[winningNr].length;
+
+      for (uint8 i = 0; i < playersByTicket[winningNr].length; i++){
+        playersByTicket[winningNr][i].transfer(amountPerAddress);
+      }
+
+    } else {
+      /* no winner*/
+    }
+  }
+
+  function printWinnerAccount() public view returns (address payable[] memory) {
+    return playersByTicket[winningNr];
+  }
 }
 
 contract RandomNumberGenerator {
