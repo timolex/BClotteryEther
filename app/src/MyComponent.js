a2396e9a0aebaaccea833deda397d6ad47171aad
Jonathan Stahl <jonathan.stahl@uzh.ch>
2019-05-14 23:47:46 +0200

    handle lottery states

--- app/src/MyComponent.js
+++ app/src/MyComponent.js
@@ -9,14 +9,21 @@ import PropTypes from 'prop-types'
 import logo from "./logo.png";
 
 class MyComponent extends React.Component {
+  state = {
+    isOwner: null,
+    dataKeyLotteryState: null // 0: closed, 1: open
+  }
 
   constructor(props, context) {
     super(props);
     this.contracts = context.drizzle.contracts;
-    this.state = {
-      isOwner: false
-    }
+  }
+
+  componentDidMount() {
     this.setIsOwner();
+    // Subscribe to lotteryState
+    const dataKeyLotteryState = this.contracts.Lottery.methods.lotteryState.cacheCall();
+    this.setState({ dataKeyLotteryState });
   }
 
   setIsOwner() {
@@ -44,6 +51,12 @@ class MyComponent extends React.Component {
   }
 
   render() {
+    // Get lotteryState from cache
+    let lotteryState = 0;
+    if (this.props.Lottery.lotteryState[this.state.dataKeyLotteryState]) {
+      lotteryState = this.props.Lottery.lotteryState[this.state.dataKeyLotteryState].value;
+    }
+
     return (
       <div className="App">
         <div>
@@ -58,61 +71,77 @@ class MyComponent extends React.Component {
 
         <div className="section">
           <h2>Lottery</h2>
-          <p>Choose a number for the lottery (1 Ether):</p>
-          <ContractForm
-            contract="Lottery"
-            method="buyTicket"
-            sendArgs={{value: 1000000000000000000}}
-            render={({ inputs, inputTypes, state, handleInputChange, handleSubmit })=> (
-              <form onSubmit={handleSubmit}>
-                {inputs.map((input, index) => (
-                  <>
-                  <input
-                    style={{ fontSize: 18, width: 80}}
-                    key={input.name} type={inputTypes[index]}
-                    name={input.name}
-                    value={state[input.name]}
-                    placeholder="Number"
-                    onChange={handleInputChange}
-                    min="1"
-                    max="250"
-                  />
-                  <button
-                    style={{ marginLeft: 10}}
-                    key="submit"
-                    type="button"
-                    onClick={handleSubmit}
-                    >
-                    Enter
-                  </button>
-                  </>
-                ))}
-              </form>
-            )}
-          />
-          <p> Your lottery tickets:</p>
-          <ContractData contract="Lottery" method="getOwnTickets" />
-        </div>
-
-        <div className="section">
-          <h3>Pot</h3>
-          <p>Amount of the jackpot (Ether):&nbsp;
-            <ContractData contract="Lottery" method="getPotOfLotteryRound" />
-          </p>
+          {lotteryState === "0" &&
+            <h3>is closed</h3>
+          }
+          {lotteryState === "1" &&
+          <div>
+            <p>Choose a number for the lottery (1 Ether):</p>
+            <ContractForm
+              contract="Lottery"
+              method="buyTicket"
+              sendArgs={{value: 1000000000000000000}}
+              render={({ inputs, inputTypes, state, handleInputChange, handleSubmit })=> (
+                <form onSubmit={handleSubmit}>
+                  {inputs.map((input, index) => (
+                    <div>
+                    <input
+                      style={{ fontSize: 18, width: 80}}
+                      key={input.name} type={inputTypes[index]}
+                      name={input.name}
+                      value={state[input.name]}
+                      placeholder="Number"
+                      onChange={handleInputChange}
+                      min="1"
+                      max="250"
+                    />
+                    <button
+                      style={{ marginLeft: 10}}
+                      key="submit"
+                      type="button"
+                      onClick={handleSubmit}
+                      >
+                      Enter
+                    </button>
+                    </div>
+                  ))}
+                </form>
+              )}
+            />
+            <p> Your lottery tickets:</p>
+            <ContractData contract="Lottery" method="getOwnTickets" />
+            <div className="section">
+              <h3>Pot</h3>
+              <p>Amount of the jackpot (Ether):&nbsp;
+                <ContractData contract="Lottery" method="getPotOfLotteryRound" />
+                </p>
+                </div>
+          </div>
+          }
         </div>
 
         {this.state.isOwner &&
           <div className="section">
-            <h3>Winners</h3>
-            <p>
-              <button onClick={this.drawWinners.bind(this)}>Draw Winners</button>
-            </p>
-            <p>Winners:&nbsp;
-              <ContractData contract="Lottery" method="printWinnerAccount" />
-            </p>
-            <p>Amount won:&nbsp;
-              <ContractData contract="Lottery" method="amountPerAddress" />
-            </p>
+            <h3>Options</h3>
+            {lotteryState === "1" &&
+              <p>
+                <button onClick={this.drawWinners.bind(this)}>Draw Winners</button>
+              </p>
+            }
+            {lotteryState === "0" &&
+              <div>
+                <p>Winners:</p>
+                <ContractData contract="Lottery" method="printWinnerAccount" />
+                <p>
+                  Amount won:&nbsp;
+                  <ContractData contract="Lottery" method="amountPerAddress" />
+                </p>
+                <p>
+                  Open Lottery:
+                  <ContractForm contract="Lottery" method="openLottery" />
+                </p>
+              </div>
+            }
           </div>
         }
 
--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,51 +1,53 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 contract Lottery {
-  enum State { Closed, Open } // Define states as enum
+  enum State { Closed, Open }
 
-  State lotteryState;
+  State public lotteryState;
   mapping(address => uint32[]) soldTickets;                // Map holding all tickets by address
   mapping(uint32 => address payable[]) playersByTicket;    // Map holding all addresses by ticket
-  uint256 public amountPerAddress;    //Winnable amount per player.
-  uint32 public winningNr;            //Winning number
-  uint32[] allPlayedNumbers;          //Array of all played numbers
-  address payable[] allPlayers;       //Array of all players
-  address payable[] winners;          //Array of winners.
-  address public owner;                      //Address of the contract owner
+  uint256 public amountPerAddress;    // Winnable amount per player.
+  uint32 public winningNr;            // Winning number
+  uint32[] allPlayedNumbers;          // Array of all played numbers
+  address payable[] allPlayers;       // Array of all players
+  address payable[] winners;          // Array of winners.
+  address public owner;               // Address of the contract owner
 
-  //Constructor which is called directly after deploying the contract.
+  // Constructor which is called directly after deploying the contract.
   constructor() public {
-    lotteryState = State.Closed;
+    lotteryState = State.Open;
     owner = msg.sender;
   }
 
-  //Here players can buy tickets when the inserted number is bewtween 1 and 250 and 1Ether is stake
+  modifier onlyOwner {
+    require(msg.sender == owner, "Only owner can call this function.");
+    _;
+  }
+
+  // Here players can buy tickets when the inserted number is bewtween 1 and 250 and 1Ether is stake
   function buyTicket(uint32 _ticketNr) public payable {
-    require(_ticketNr < 251 && _ticketNr > 0, "Ticket number must be in range [0, 250]");   // Prevent overflow of uint32
+    require(lotteryState == State.Open, "Lottery is closed");
+    require(_ticketNr < 251 && _ticketNr > 0, "Ticket number must be in range [0, 250]");
     require(msg.value >= 1, "A ticket costs 1 Ether");    // Checks if enough ether is sent to buy a lottery ticket
 
-    //Check if PLayer already played --> Prevention of duplicates in allPlayersArray.
+    // Check if PLayer already played --> Prevention of duplicates in allPlayersArray.
     if (soldTickets[msg.sender].length == 0) {
       allPlayers.push(msg.sender);
     }
 
-    //Add Addresses and ticketnumer to the correct mappings and arrays
+    // Add Addresses and ticketnumer to the correct mappings and arrays
     soldTickets[msg.sender].push(_ticketNr);
     playersByTicket[_ticketNr].push(msg.sender);
     allPlayedNumbers.push(_ticketNr);
   }
 
-  //Get tickets played by a user
+  // Get tickets played by a user
   function getOwnTickets() public view returns (uint32[] memory) {
     return soldTickets[msg.sender];
   }
 
-  function getLotteryState() public view returns (State) {
-    return lotteryState;
-  }
-
-  function setLotteryState(State _lotteryState) public {
-    lotteryState = _lotteryState;
+  function openLottery() public onlyOwner {
+    lotteryState = State.Open;
   }
 
   function getRandomNumberFromOracle(address _oracleAddress) public returns (uint32) {
@@ -57,11 +59,13 @@ contract Lottery {
     return (address(this).balance/(1 ether));
   }
 
-  function drawWinners(address _oracleAddress) public {
+  function drawWinners(address _oracleAddress) public onlyOwner {
+    lotteryState = State.Closed;
+    
     winningNr = getRandomNumberFromOracle(_oracleAddress);
 
     if (playersByTicket[winningNr].length > 0) {
-      //Get the won amount per winner
+      // Get the won amount per winner
       amountPerAddress = address(this).balance / playersByTicket[winningNr].length;
 
       for (uint32 i = 0; i < playersByTicket[winningNr].length; ++i){
@@ -70,35 +74,35 @@ contract Lottery {
       }
     }
 
-    //Empty the mappings for a new round --> May move this to the state where a new round is opened.
+    // Empty the mappings for a new round --> May move this to the state where a new round is opened.
     emptyMapping();
   }
 
-  //Used to print the winners in the frontend.
+  // Used to print the winners in the frontend.
   function printWinnerAccount() public view returns (address payable[] memory) {
     return winners;
   }
 
-  //Emptying of the mappings and arrays, such that a new round can be started.
+  // Emptying of the mappings and arrays, such that a new round can be started.
   function emptyMapping() private {
 
-    //delete the playersByTicket mapping
-    for (uint256 i = 0; i<allPlayedNumbers.length; ++i){
+    // Delete the playersByTicket mapping
+    for (uint256 i = 0; i < allPlayedNumbers.length; ++i){
       delete playersByTicket[allPlayedNumbers[i]];
     }
 
-    //delete the soldTickets mapping
-    for (uint256 i = 0; i<allPlayers.length; ++i){
+    // Delete the soldTickets mapping
+    for (uint256 i = 0; i < allPlayers.length; ++i){
       delete soldTickets[allPlayers[i]];
     }
 
-    //Delete the two helper arrays.
+    // Delete the two helper arrays.
     delete allPlayedNumbers;
     delete allPlayers;
   }
 }
 
-//Interface
+// Interface
 contract RandomNumberGenerator {
   function getRandomNumber() public returns (uint32);
 }
